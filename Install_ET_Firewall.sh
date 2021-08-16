074beeb27c3a0b377d2215802ad8c618f7429939
alexlewislnk <39069234+alexlewislnk@users.noreply.github.com>
2021-08-16 09:24:08 -0500

    Update Install_ET_Firewall.sh

--- Install_ET_Firewall.sh
+++ Install_ET_Firewall.sh
@@ -6,40 +6,58 @@
 # faster matching in the firewall. 
 #
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+LOG=/root/Install_ET_Firewall.log
+RED="$(tput setaf 1)"
+YELLOW="$(tput setaf 3)"
+CYAN="$(tput setaf 6)"
+NORMAL="$(tput sgr0)"
 
-echo "Installing firewalld"
-apt -y install firewalld ipset libnet-ip-perl
+function DisplayInfo { 
+	printf "${CYAN}$INFO${NORMAL}\\n"
+	printf "$INFO\\n" >>$LOG
+}
 
-echo "Make sure ssh (tcp/22) is allowed"
-firewall-cmd --zone=public --add-service=ssh --permanent
-firewall-cmd --reload
+INFO="Firewalld setup started at $(date)" ; DisplayInfo
 
-echo "Download scripts to load Emerging Threats and Geo Blocklists"
-wget -O /usr/local/bin/ip-aggregate.pl https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ip-aggregate.pl
-wget -O /usr/local/bin/emerging-threats-update.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/emerging-threats-update.sh
-wget -O /usr/local/bin/country-block.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/country-block.sh
-wget -O /usr/local/bin/ipset-backup.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ipset-backup.sh
-wget -O /usr/local/bin/ipset-restore.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ipset-restore.sh
-chmod +rx /usr/local/bin/ip-aggregate.pl /usr/local/bin/emerging-threats-update.sh /usr/local/bin/country-block.sh /usr/local/bin/ipset-backup.sh /usr/local/bin/ipset-restore.sh
+INFO="Installing firewalld" ; DisplayInfo
+apt update >>$LOG 2>&1
+apt -y install firewalld ipset libnet-ip-perl >>$LOG 2>&1
 
-echo "Create ipset lists and add blocklists to firewalld"
-firewall-cmd --permanent --new-ipset=threat-ip --type=hash:ip
-firewall-cmd --permanent --new-ipset=threat-net --type=hash:net
-firewall-cmd --permanent --new-ipset=geo-block --type=hash:net
-firewall-cmd --permanent --add-rich-rule='rule source ipset=threat-ip log prefix="THREAT BLOCK:" drop'
-firewall-cmd --permanent --add-rich-rule='rule source ipset=threat-net log prefix="THREAT BLOCK:" drop'
-firewall-cmd --permanent --add-rich-rule='rule source ipset=geo-block log prefix="GEO BLOCK:" drop'
-firewall-cmd --reload
 
-echo "Run initial download of the blocklists"
-/usr/local/bin/emerging-threats-update.sh > /dev/null 2>&1
-/usr/local/bin/country-block.sh > /dev/null 2>&1
-/usr/local/bin/ipset-backup.sh
+INFO="Make sure ssh (tcp/22) is allowed" ; DisplayInfo
+firewall-cmd --zone=public --add-service=ssh --permanent >>$LOG 2>&1
+firewall-cmd --reload >>$LOG 2>&1
 
-echo "Setup crontab to regular update and backup the blocklists and restore on reboot"
-(crontab -l ; echo "@hourly /usr/local/bin/emerging-threats-update.sh" )| crontab -
-(crontab -l ; echo "@daily /usr/local/bin/country-block.sh" )| crontab -
-(crontab -l ; echo "15 * * * * /usr/local/bin/ipset-backup.sh" )| crontab -
-(crontab -l ; echo "@reboot sleep 60 ; /usr/local/bin/ipset-restore.sh" )| crontab -
+INFO="Download scripts to load Emerging Threats and Geo Blocklists" ; DisplayInfo
+wget -O /usr/local/bin/ip-aggregate.pl https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ip-aggregate.pl >>$LOG 2>&1
+wget -O /usr/local/bin/emerging-threats-update.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/emerging-threats-update.sh >>$LOG 2>&1
+wget -O /usr/local/bin/country-block.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/country-block.sh >>$LOG 2>&1
+wget -O /usr/local/bin/ipset-backup.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ipset-backup.sh >>$LOG 2>&1
+wget -O /usr/local/bin/ipset-restore.sh https://raw.githubusercontent.com/alexlewislnk/ET-Firewalld/main/ipset-restore.sh >>$LOG 2>&1
+chmod +rx /usr/local/bin/*.pl /usr/local/bin/*.sh >>$LOG 2>&1
+
+
+INFO="Create ipset lists and add blocklists to firewalld" ; DisplayInfo
+firewall-cmd --permanent --new-ipset=threat-ip --type=hash:ip >>$LOG 2>&1
+firewall-cmd --permanent --new-ipset=threat-net --type=hash:net >>$LOG 2>&1
+firewall-cmd --permanent --new-ipset=geo-block --type=hash:net >>$LOG 2>&1
+firewall-cmd --permanent --add-rich-rule='rule source ipset=threat-ip log prefix="THREAT BLOCK:" drop' >>$LOG 2>&1
+firewall-cmd --permanent --add-rich-rule='rule source ipset=threat-net log prefix="THREAT BLOCK:" drop' >>$LOG 2>&1
+firewall-cmd --permanent --add-rich-rule='rule source ipset=geo-block log prefix="GEO BLOCK:" drop' >>$LOG 2>&1
+firewall-cmd --reload >>$LOG 2>&1
+
+INFO="Run initial download of the blocklists" ; DisplayInfo
+/usr/local/bin/emerging-threats-update.sh >>$LOG 2>&1
+/usr/local/bin/country-block.sh >>$LOG 2>&1
+/usr/local/bin/ipset-backup.sh >>$LOG 2>&1
+
+INFO="Setup crontab to regularly update and backup the blocklists and restore on reboot" ; DisplayInfo
+(crontab -l ; echo "@hourly /usr/local/bin/emerging-threats-update.sh" )| crontab - >>$LOG 2>&1
+(crontab -l ; echo "@daily /usr/local/bin/country-block.sh" )| crontab - >>$LOG 2>&1
+(crontab -l ; echo "15 * * * * /usr/local/bin/ipset-backup.sh" )| crontab - >>$LOG 2>&1
+(crontab -l ; echo "@reboot sleep 60 ; /usr/local/bin/ipset-restore.sh" )| crontab - >>$LOG 2>&1
+
+INFO="Firewalld setup completed at $(date)" ; DisplayInfo
+INFO="Check log file $LOG for any errors" ; DisplayInfo
 
 # End
